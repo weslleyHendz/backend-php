6444792670796ac14a1473683b94173a148e48f3
Weslley Da Costa Hendz <58227073+weslleyHendz@users.noreply.github.com>
2021-12-26 17:20:42 -0300

    FrontEnd implementado para facilitar o uso/visualização

--- main.php
+++ main.php
@@ -1,50 +1,128 @@
-<?php
+<!doctype html>
+<html lang="en">
 
-require_once __DIR__ . '/vendor/autoload.php';
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body{
+            width: 100%;
+            height: 100%;
+            background-color: #e7e7e7 !important;
+        }
+        .container {
+            padding: 20px;
+        }
+        .card{
+            background: linear-gradient(to bottom, #e2e2e2, #fff);
+            width: 50%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .php-code{
+            margin: 10px;
+        }
+        .btn-color{
+            background-color: #e7e7e7 !important;
+            color: black !important;
+            border: 1px solid #000 !important;
+        }
+        .btn-color:hover{
+            background-color: #858585 !important;
+            color: white !important;
+        }
+        .form-width{
+            vertical-align: middle;
+            padding: .375rem .75rem;
+        }
+        .form-label{
+            font-weight: auto;
+        }
+        </style>
+        <title>Backend - PHP - Mooving</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <form action="main.php" method="post">
+                <div class="my-3 php-code" >
+                    <p>
+                    <h3>
+                        Caixa Eletrônico - Mooving
+                    </h3>
+                   <b> Limite de saque:</b><br/>
+                    Conta Corrente: B$ 600;<br/>
+                    Poupança: B$ 1.000.
+                    </p>
+                   <b> Saldo em conta: B$</b>
+                    <?php 
+                    if (!isset($saldoConta)){
+                        $saldoConta = 1200;
+                    }
+                    echo $saldoConta
+                    ?>
+                    <br/>
+                    <div class="my-3">
+                        <b>Taxas para saque:<br/></b>
+                        <p>
+                            Conta Corrente: B$ 2,50;<br/>
+                            Conta Poupança: B$ 0,80;
+                        </p>
+                    </div>
+                    <select class="form-select" aria-label="Default select example" id = 'operation' name = 'operation'>
+                        <option selected>Selecione o tipo de transação:</option>
+                        <option value="saqueCC">Saque Conta Corrente</option>
+                        <option value="saquePoupanca">Saque Poupança</option>
+                        <option value="deposito">Depósito</option>
+                        <option value="transferencia">Transferência</option>
+                    </select>
+                    <label class="form-label ">Valor da operação:</label>
 
-use Moovin\Job\Backend;
+                    <input  class="form-control" required="true"  name = "value" method="POST"/>
+                    <button typ
+                    e="submit" class="btn btn-color">Enviar</button>
 
-$exemplo = new Backend\cashMachine;
+                </div>
 
-// Saque Poupança: Limite de B$1000 e taxa de B$0.80
-// $accountABC = [
-//     'type' => 'saquePoupanca',
-//     'valueTotal' => 1200,
-//     'value' => 900
-// ];
+                <div class="php-code">
+                <?php
+                require "./src/cashMachine.php";
 
-// Saque Conta Corrente: Limite de B$600 e taxa de B$2.50
-// $accountABC = [
-//     'type' => 'saqueCC',
-//     'valueTotal' => 1200,
-//     'value' => 900
-// ];
-
-// Operação de depósito
-// $accountABC = [
-//     'type' => 'deposito',
-//     'valueTotal' => 1200,
-//     'value' => 900
-// ];
-
-// Operação de transferência
-$accountABC = [
-    'type' => 'transferência',
-    'valueTotal' => 1200,
-    'value' => 900
-];
-
-$accountXYZ = [
-    'name' =>'Weslley Hendz',
-    'valueTotal' => 1200,
-    'value' => 900
-];
-// $contType = 'deposito';
-// $valueTotal = 1200;
-// $valueDraft = 900;
-if (isset($accountXYZ)){
-    echo $exemplo->bankDraft($accountABC, $accountXYZ);
-}else{
-    echo $exemplo->bankDraft($accountABC);
-
-}
+                // $saldoConta = 1200;
+                $operation = filter_input(INPUT_POST, 'operation', FILTER_SANITIZE_STRING);
+                $accountXYZ = [];
+                if($operation == 'transferencia'){
+                    $accountXYZ=[
+                        'valueTotal'=>2000,
+                        'name'=> 'Weslley Hendz',            
+                    ];
+                }
+                if (isset(($_POST['value'])) and isset($operation)) {
+                    //Ex:
+                    // Saque Poupança: Limite de B$1000 e taxa de B$0.80
+                    // $accountABC = [
+                    //     'type' => 'saquePoupanca',
+                    //     'valueTotal' => 1200,
+                    //     'value' => 900
+                    // ];
+                    $value = [];
+                    $value =
+                        [
+                        'type' => $operation,
+                        'valueTotal' => $saldoConta,
+                        'value' => $_POST['value'],
+                    ];
+                    $request = new cashMachine();
+                    $print = $request->electronicBank($value, $accountXYZ);
+                    echo $print;
+                }
+                ?>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- src/cashMachine.php
+++ src/cashMachine.php
@@ -1,6 +1,8 @@
+
+
 <?php
 
-namespace Moovin\Job\Backend;
+// namespace Moovin\Job\Backend;
 
 /**
  * Classe que trata as contas correntes
@@ -8,47 +10,44 @@ namespace Moovin\Job\Backend;
  * @author Weslley Hendz <hendzweslley@gmail.com>
  */
 
-class cashMachine 
+class cashMachine
 {
-
-    public function bankDraft($accountABC, $accountXYZ=[])
+    public function electronicBank($accountABC, $accountXYZ = [])
     {
-        if($accountABC['type'] == 'saquePoupanca'){
+        if ($accountABC['type'] == 'saquePoupanca') {
             $limit = 1000;
             $tax = 0.80;
         }
-        if($accountABC['type'] == 'saqueCC'){
+        if ($accountABC['type'] == 'saqueCC') {
             $limit = 600;
             $tax = 2.50;
         }
-        if($accountABC['value'] > $accountABC['valueTotal']){
+        if ($accountABC['value'] > $accountABC['valueTotal']) {
             return "Saldo insuficiente. Saldo atual: B$" . $accountABC['valueTotal'];
         }
-        if(isset($limit) AND $accountABC['value'] > $limit){
-            return "O limite para saques neste tipo de conta é B$ ". $limit . ". \nVocê tentou sacar: B$ " . $accountABC['value'];
+        if (isset($limit) and $accountABC['value'] > $limit) {
+            return "O limite para saques neste tipo de conta é B$ " . $limit . ". <br>Você tentou sacar: B$ " . $accountABC['value'];
         }
-        if(isset($limit) AND $accountABC['value'] <= $accountABC['valueTotal'] AND $accountABC['value'] <= $limit){
+        if (isset($limit) and $accountABC['value'] <= $accountABC['valueTotal'] and $accountABC['value'] <= $limit) {
             $balance = bcsub(bcsub($accountABC['valueTotal'], $accountABC['value'], 2), $tax, 2);
-
-            return "Saque efetuado no valor de B$: " . $accountABC['value'] . ". Saldo atual em conta: " . $balance;
+            return "Saque efetuado no valor de B$: " . $accountABC['value'] . ". Saldo atual em conta: B$" . $balance;
         }
-        if($accountABC['type'] == 'deposito'){
+        if ($accountABC['type'] == 'deposito') {
             $balance = bcadd($accountABC['valueTotal'], $accountABC['value'], 2);
-            return "Depósito efetuado com sucesso no valor de B$ " . $accountABC['value'] . ". \nSaldo anterior ao depósito: B$ " . $accountABC['valueTotal'] . "\nSaldo atual em conta: " . $balance;
+            return "Depósito efetuado com sucesso no valor de B$ " . $accountABC['value'] . ". <br>Saldo anterior ao depósito: B$ " . $accountABC['valueTotal'] . "<br>Saldo atual em conta: B$ " . $balance;
         }
-        if($accountABC['type']  == 'transferência' AND $accountABC['value'] >= $accountABC['valueTotal']){
+        if ($accountABC['type'] == 'transferencia' and $accountABC['value'] > $accountABC['valueTotal']) {
             return "Saldo insuficiente. Saldo atual: B$" . $accountABC['valueTotal'];
         }
-        if($accountABC['type']  == 'transferência' AND $accountABC['value'] <= $accountABC['valueTotal']){
+        if ($accountABC['type'] == 'transferencia' and $accountABC['value'] <= $accountABC['valueTotal']) {
             $balanceAccountABC = bcsub($accountABC['valueTotal'], $accountABC['value'], 2);
             $balanceAccountXYZ = bcadd($accountXYZ['valueTotal'], $accountABC['value'], 2);
-
-            return "Transferência efetuada com sucesso no valor de B$ " . $accountABC['value'] . ". 
-            \nSaldo atual de B$ " . $balanceAccountABC . "
-            \nNome do recebedor: " . $accountXYZ['name'] . "
-            \nSaldo anterior:  " . $accountXYZ['valueTotal'] . "
-            \nSaldo atual do recebedor:" . $balanceAccountXYZ;
+            return 
+            "Transferência efetuada com sucesso no valor de B$ " . $accountABC['value'] . ".
+            <br>Saldo atual: B$ " . $balanceAccountABC . "
+            <br>Nome do recebedor: " . $accountXYZ['name'] . "
+            <br>Saldo anterior: B$  " . $accountXYZ['valueTotal'] . "
+            <br>Saldo atual do recebedor:" . $balanceAccountXYZ;
         }
     }
-
 }
